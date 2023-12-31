2b064bee6a9e02c5260f394722add74079c4f309
parsa-official <parsa-official>
2023-12-31 15:47:38 +0330

    test

--- GC.py
+++ GC.py
@@ -3,25 +3,27 @@ from PIL import Image
 import streamlit as st
 import json
 
-def load_config(file_path="config.json"):
-    with open(file_path, "r") as f:
-        config = json.load(f)
-    return config
+st.write('test')
 
-config = load_config()
-api_key = config.get("api_key")
+# def load_config(file_path="config.json"):
+#     with open(file_path, "r") as f:
+#         config = json.load(f)
+#     return config
 
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-pro')
+# config = load_config()
+# api_key = config.get("api_key")
 
-txt = st.text_input(label='Enter a text:')
-question = 'Check Grammar (English) of Text and if false, Say **Incorrect** and give a Correction and say why it is incorrect. But if true, Say **Correct** and give another example like that \n Text:'
-button_clicked = st.button("Check Grammer")
-response = model.generate_content([question, txt])
+# genai.configure(api_key=api_key)
+# model = genai.GenerativeModel('gemini-pro')
+
+# txt = st.text_input(label='Enter a text:')
+# question = 'Check Grammar (English) of Text and if false, Say **Incorrect** and give a Correction and say why it is incorrect. But if true, Say **Correct** and give another example like that \n Text:'
+# button_clicked = st.button("Check Grammer")
+# response = model.generate_content([question, txt])
 
 
-# for word in response:
-if 'Incorrect' in response.text:
-    st.write(f"<p style='color: red;'>{response.text}</p>", unsafe_allow_html=True)
-else:
-    st.write(f"<p style='color: green;'>{response.text}</p>", unsafe_allow_html=True)
+# # for word in response:
+# if 'Incorrect' in response.text:
+#     st.write(f"<p style='color: red;'>{response.text}</p>", unsafe_allow_html=True)
+# else:
+#     st.write(f"<p style='color: green;'>{response.text}</p>", unsafe_allow_html=True)
