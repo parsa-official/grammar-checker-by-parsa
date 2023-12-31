1255a48dd5048a0470e8b9be4fdb16f527ecea33
parsa-official <parsa-official>
2023-12-31 15:45:11 +0330

    add main code and replace name to GC.py

--- GC.py
+++ GC.py
@@ -3,10 +3,6 @@ from PIL import Image
 import streamlit as st
 import json
 
-# API = "AIzaSyC6trLcXtcQWDhGca-l7QVDZPWh19fIYDs"
-# genai.configure(api_key=API)
-
-
 def load_config(file_path="config.json"):
     with open(file_path, "r") as f:
         config = json.load(f)
@@ -16,12 +12,8 @@ config = load_config()
 api_key = config.get("api_key")
 
 genai.configure(api_key=api_key)
-
-
-
 model = genai.GenerativeModel('gemini-pro')
 
-# img = Image.open('photo_2023-12-30_15-14-26.jpg')
 txt = st.text_input(label='Enter a text:')
 question = 'Check Grammar (English) of Text and if false, Say **Incorrect** and give a Correction and say why it is incorrect. But if true, Say **Correct** and give another example like that \n Text:'
 button_clicked = st.button("Check Grammer")
